2119c67c516df821369fafc1ac0f23b796c9078d
Созонтов Алексей <sozvic1@gmail.com>
2018-09-02 18:33:12 +0300

    Add GroupRemovalTests

--- addressbook_web_test/addressbook_web_test/CreateContactTest.cs
+++ addressbook_web_test/addressbook_web_test/CreateContactTest.cs
@@ -10,37 +10,9 @@ using OpenQA.Selenium.Support.UI;
 namespace WebAddressbookTest
 {
     [TestFixture]
-    public class Untitled2
+    public class CreateContactTest : TestBase
     {
-        private IWebDriver driver;
-        private StringBuilder verificationErrors;
-        private string baseURL;
-        private bool acceptNextAlert = true;
-
-        [SetUp]
-        public void SetupTest()
-        {
-            FirefoxOptions options = new FirefoxOptions();
-            options.BrowserExecutableLocation = @"c:\Program Files\Mozilla Firefox\firefox.exe";
-            options.UseLegacyImplementation = true;
-            driver = new FirefoxDriver(options);
-            baseURL = "http://localhost/";
-            verificationErrors = new StringBuilder();
-        }
-
-        [TearDown]
-        public void TeardownTest()
-        {
-            try
-            {
-                driver.Quit();
-            }
-            catch (Exception)
-            {
-                // Ignore errors if unable to close the browser
-            }
-            Assert.AreEqual("", verificationErrors.ToString());
-        }
+       
 
         [Test]
         public void TheUntitled2Test()
@@ -98,71 +70,6 @@ namespace WebAddressbookTest
           
         }
 
-        private void LogOut()
-        {
-            driver.FindElement(By.LinkText("LOGOUT")).Click();
-        }
-
-        private void Login(AccountData login)
-        {
-            driver.FindElement(By.Name("user")).Clear();
-            driver.FindElement(By.Name("user")).SendKeys(login.Login);
-            driver.FindElement(By.Name("pass")).Clear();
-            driver.FindElement(By.Name("pass")).SendKeys(login.Password);
-            driver.FindElement(By.CssSelector("input[type=\"submit\"]")).Click();
-        }
-
-        private void OpenHomePage()
-        {
-            driver.Navigate().GoToUrl(baseURL + "addressbook/edit.php");
-        }
-
-        private bool IsElementPresent(By by)
-        {
-            try
-            {
-                driver.FindElement(by);
-                return true;
-            }
-            catch (NoSuchElementException)
-            {
-                return false;
-            }
-        }
-
-        private bool IsAlertPresent()
-        {
-            try
-            {
-                driver.SwitchTo().Alert();
-                return true;
-            }
-            catch (NoAlertPresentException)
-            {
-                return false;
-            }
-        }
-
-        private string CloseAlertAndGetItsText()
-        {
-            try
-            {
-                IAlert alert = driver.SwitchTo().Alert();
-                string alertText = alert.Text;
-                if (acceptNextAlert)
-                {
-                    alert.Accept();
-                }
-                else
-                {
-                    alert.Dismiss();
-                }
-                return alertText;
-            }
-            finally
-            {
-                acceptNextAlert = true;
-            }
-        }
-    }
+      
+     }
 }
---
+++ addressbook_web_test/addressbook_web_test/GroupRemovalTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+
+namespace WebAddressbookTest
+{
+    [TestFixture]
+    public class GroupRemovalTests : TestBase
+    {
+      
+
+        [Test]
+        public void GroupRemovalTest()
+        {
+            OpenHomePage();
+            Login(new AccountData("admin","secret"));
+            GoToGroupPage();
+            SelectGroup(1);
+            RemoveGroup();
+            RetutnToGroupPage();
+           
+        }
+
+      
+
+      
+    
+        
+
+      
+
+           
+     }
+}
