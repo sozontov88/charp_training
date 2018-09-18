5abc9bed45578194a0cbaaa6405b0c676d3c61f5
Созонтов Алексей <sozvic1@gmail.com>
2018-09-18 21:30:59 +0300

    Add Collection in GroupTest

--- addressbook_web_test/addressbook_web_test/Tests/GroupCreationTest.cs
+++ addressbook_web_test/addressbook_web_test/Tests/GroupCreationTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
@@ -17,18 +18,23 @@ namespace WebAddressbookTest
             GroupData group = new GroupData("first");
             group.Footer = "aaa";
             group.Header = "zzz";
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
             app.Groups.Create(group);
-            Assert.IsTrue(app.Groups.IsGroupExist(group));
-        }
-        //[Test]
-        //public void EmptyGroupCreationTests()
-        //{
-        //    GroupData group = new GroupData("");
-        //    group.Footer = "";
-        //    group.Header = "";
-        //    app.Groups.Create(group);
-        //    Assert.IsTrue(app.Groups.IsGroupExist(group));
+            List<GroupData> newGroups = app.Groups.GetGroupList();
 
-        //}
+            Assert.AreEqual(oldGroups.Count+1,newGroups.Count);
+        }
+        [Test]
+        public void EmptyGroupCreationTests()
+        {
+            GroupData group = new GroupData("");
+            group.Footer = "";
+            group.Header = "";
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+            app.Groups.Create(group);
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+            Assert.AreEqual(oldGroups.Count + 1, newGroups.Count);
+
+        }
     }
 }
--- addressbook_web_test/addressbook_web_test/appmanager/GroupHelper.cs
+++ addressbook_web_test/addressbook_web_test/appmanager/GroupHelper.cs
@@ -26,6 +26,19 @@ namespace WebAddressbookTest
             return this;
         }
 
+        public List<GroupData> GetGroupList()
+        {
+            List< GroupData> groups = new List< GroupData>();
+            manager.Navigator.GoToGropsPage();
+            ICollection<IWebElement> elements = driver.FindElements(By.CssSelector("span.group"));
+                foreach(IWebElement element in elements)
+            {
+                
+                groups.Add(new GroupData(element.Text));
+            }
+            return groups;
+        }
+
         public GroupHelper Modify(int p, GroupData group)
         {
             manager.Navigator.GoToGropsPage();
